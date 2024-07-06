3687725a5f16d2dd9e634f68a802c2ca59c61a21
Abdul3147 <abdallahussein228@gmail.com>
2024-07-06 22:11:38 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = "bob" ;
+console.log(customerName) ;
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName) ;
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+console.log(setBestCustomer) ;
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = "maybe bob";
+
+}
+console.log(overwriteBestCustomer) ;
+
+//
+
+//console.log(changeLeastFavoriteCustomer) ;
+const leastFavoriteCustomer = "Kim"
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = "bob"
+}
