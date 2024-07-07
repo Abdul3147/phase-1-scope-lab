5f5c7cdcdb7297a05e0d9b3462dde26100e4b3f6
Abdul3147 <abdallahussein228@gmail.com>
2024-07-07 17:36:58 +0300

    done

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 // Write your solution in this file!
-var customerName = "bob" ;
-console.log(customerName) ;
+var customerName = "bob";
 
 function upperCaseCustomerName() {
-  window.customerName = window.customerName.toUpperCase();
+   window.customerName = window.customerName.toUpperCase();
 }
-console.log(upperCaseCustomerName) ;
-
-function setBestCustomer() {
-  window.bestCustomer = "not bob";
+function setBestCustomer(){
+   window.bestCustomer = "not bob";
 }
-console.log(setBestCustomer) ;
-
-function overwriteBestCustomer(newBestCustomer) {
-  window.bestCustomer = "maybe bob";
-
+function overwriteBestCustomer(){
+   window.bestCustomer = "maybe bob";
 }
-console.log(overwriteBestCustomer) ;
-
-//
-
-//console.log(changeLeastFavoriteCustomer) ;
 const leastFavoriteCustomer = "Kim"
 function changeLeastFavoriteCustomer(){
    leastFavoriteCustomer = "bob"
